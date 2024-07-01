0514b357f2667c550d0616c7db5893770c898660
Charlie Brewis <charliebrewis7@gmail.com>
2024-07-01 13:20:33 +0100

    Changed urls to handle our RoomSerialiser view

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -4,6 +4,8 @@ from .models import Room
 # Serialisers translate python model classes into JSON data
 # This enables our frontend to read from these models very easily
 
+# Inherits from the ModelSerializer class from the rest_framework module
+# This class is used to serialize and deserialize Django models
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics
 from .models import Room
 from .serializers import RoomSerializer
 
+# A view is a function that takes a web request and returns a web response
+# These are also known as request handlers or request-response handlers
+
+
 # This is a view that inherits from the CreateAPIView class from the rest_framework module
 # This class is used to create a new room or return to us a list of all rooms
 class RoomView(generics.CreateAPIView):
