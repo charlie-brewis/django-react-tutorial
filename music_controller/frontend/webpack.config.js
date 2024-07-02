60a20c23c0e362896ef1cf5d83fade3c223a91d4
Charlie Brewis <charliebrewis7@gmail.com>
2024-07-02 12:38:43 +0100

    Added webpack configuration

---
+++ music_controller/frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");
+// Webpack compiles all js files into 1 file that can be run on the browser
+module.exports = {
+    // Entry js file is here
+    entry: "./src/index.js",
+    // Output combined file here
+    output: {
+        path: path.resolve(__dirname, "./static/frontend"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                // This has effect on the react lib size
+                NODE_ENV: JSON.stringify("production"),
+            },
+        }),
+    ],
+};
