66bd9cb14bcb412b7b623c8532fd25100d2e6370
Charlie Brewis <charliebrewis7@gmail.com>
2024-07-02 14:44:22 +0100

    Bundled js with npm run dev

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -14,4 +14,4 @@ export default class App extends Component {
 const appDiv = document.getElementById("app");
 // Create a react root inside the appDiv so we can render our react components
 const root = ReactDOM.createRoot(appDiv);
-root.render(<App />)
+root.render(<App />);
