59c7406bcb87cd0355fccb99c32240d01691f2f9
Charlie Brewis <charliebrewis7@gmail.com>
2024-07-05 16:34:11 +0100

    Added HomePage frontend

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import RoomCreatePage from "./RoomCreatePage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom";
 
 export default class HomePage extends Component {
@@ -9,12 +10,34 @@ export default class HomePage extends Component {
         super(props);
     }
 
+    renderHomePage() {
+        return (
+            <Grid container spacing={3} align="center">
+                <Grid item xs={12}>
+                    <Typography variant="h3" component="h4">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={Link}>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={Link}>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render() {
         return (
             // This block acts like a html switch statement, routing to each route depending on the url
             <Router>
                 <Routes>
-                    <Route path="/" element={<p>This is the home page</p>} />
+                    <Route path="/" element={this.renderHomePage()} />
                     <Route path="/join" element={<RoomJoinPage />} />
                     <Route path="/create" element={<RoomCreatePage />} />
                     <Route path="/room/:roomCode" element={<Room />} />
