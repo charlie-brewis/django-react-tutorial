7ad16491062044c3a0887aa6ec7c3ff312a5c435
Charlie Brewis <charliebrewis7@gmail.com>
2024-07-08 15:56:27 +0100

    Refactored imports

--- music_controller/frontend/src/components/RoomCreatePage.js
+++ music_controller/frontend/src/components/RoomCreatePage.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
 import { Link } from "react-router-dom";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import withNavigate from "./higherOrderComponents/withNavigate";
+import {
+    Button,
+    Grid,
+    Typography,
+    FormHelperText,
+    FormControl,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+} from "@mui/material";
 
 class RoomCreatePage extends Component {
     defaultVotes = 2;
